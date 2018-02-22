ae7eeee49b65f173542d8ad22cf6aa6ccff8dc3b
yilingkuo <lyn3023298@gmail.com>
2018-02-22 10:32:12 -0500

    using express static routes. css and image are readible now.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app=express();
 
+//serve static files -e.g. html, css
+app.use(express.static(__dirname));
+
 var https=require('https');
 var fs=require('fs')
 var privateKey = fs.readFileSync('/home/studentuser/certs/client-key.pem').toString();
@@ -8,7 +11,7 @@ var certificate = fs.readFileSync('/home/studentuser/certs/client-cert.pem').toS
 var credentials ={key: privateKey, cert: certificate};
 var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(4443);
-
+/*
 app.get('/:fileName',function(req,res){
 	//run some server-side code
 	var fileName = req.params.fileName;
@@ -16,3 +19,4 @@ app.get('/:fileName',function(req,res){
 	//note that __dirname gives the path to the server.js file
 	res.sendFile(__dirname + '/' + fileName); 
 });
+*/
